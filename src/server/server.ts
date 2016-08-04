4996abaa390163d4ace9c5f8db8a9d7fd3e1b7c5
Ron Borysovski <roni537@gmail.com>
2016-08-04 21:10:30 +0300

    Fixed REST API routing

--- src/server/server.ts
+++ src/server/server.ts
@@ -5,7 +5,8 @@ import * as bodyParser from 'body-parser';
 import * as morgan from 'morgan';
 import * as methodOverride from 'method-override';
 import * as mongoose from 'mongoose';
-import * as requestsRouter from './controllers/requests.controller'; 
+import * as requestRouter from './controllers/request.controller';
+import * as userRouter from './controllers/user.controller';  
 
 const app: express.Application = express();
 
@@ -13,12 +14,14 @@ mongoose.connect('mongodb://localhost/blue-ivory-2');
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(bodyParser.json({ type: 'application/vnd.api+json'}));
 app.use(methodOverride());
 app.use(morgan('dev'));
 
 const port: number = 80;
 
-app.use('/api/request', requestsRouter);
+app.use('/api/', requestRouter);
+app.use('/api/', userRouter);
 
 var server = app.listen(port, () => {
     console.log('Application is running port %s', port);
