7f8b5df4773b724ef57c563ff0cd38d285fe5ae1
Ron Borysovski <roni537@gmail.com>
2016-10-09 16:07:42 +0300

    User auth middleware on each route endpoint

--- src/server/controllers/user.controller.ts
+++ src/server/controllers/user.controller.ts
@@ -3,11 +3,12 @@
 import * as express from 'express';
 import { UserManager } from './../managers/user.manager';
 import { User } from './../classes/user';
+import { AuthMiddleware } from './../middlewares/auth.middleware';
 
 var router: express.Router = express.Router();
 var userManager = new UserManager();
 
-router.get('/user', (req: express.Request, res: express.Response) => {
+router.get('/user', AuthMiddleware.requireLogin, (req: express.Request, res: express.Response) => {
     userManager.all().then((users) => {
         return res.json(users);
     }).catch((error) => {
@@ -16,7 +17,7 @@ router.get('/user', (req: express.Request, res: express.Response) => {
     });
 });
 
-router.get('/user/:id', (req: express.Request, res: express.Response) => {
+router.get('/user/:id', AuthMiddleware.requireLogin, (req: express.Request, res: express.Response) => {
 
     userManager.read(req.params.id).then((user) => {
         if (!user) {
@@ -31,7 +32,7 @@ router.get('/user/:id', (req: express.Request, res: express.Response) => {
     });
 });
 
-router.post('/user', (req: express.Request, res: express.Response) => {
+router.post('/user', AuthMiddleware.requireLogin, (req: express.Request, res: express.Response) => {
     userManager.create(req.body.user).then((user) => {
         return res.json(user);
     }).catch((error) => {
@@ -40,7 +41,7 @@ router.post('/user', (req: express.Request, res: express.Response) => {
     });
 });
 
-router.delete('/user/:id', (req: express.Request, res: express.Response) => {
+router.delete('/user/:id', AuthMiddleware.requireLogin, (req: express.Request, res: express.Response) => {
     userManager.delete(req.params.id).then((user) => {
         if (!user) {
             return res.sendStatus(404);
