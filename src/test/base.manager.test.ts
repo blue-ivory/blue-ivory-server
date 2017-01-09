eff3de1a31269fb79a5ae7eb2e4f7a740a3ec982
Ron Borysovski <roni537@gmail.com>
2017-01-09 16:45:40 +0200

    Added tests to base users count

--- src/test/base.manager.test.ts
+++ src/test/base.manager.test.ts
@@ -1,11 +1,14 @@
 /// <reference path="../../typings/index.d.ts" />
 import { BaseManager } from './../server/managers/base.manager';
+import { UserManager } from './../server/managers/user.manager';
 import { Base } from './../server/classes/base';
+import { User } from './../server/classes/user';
 import { expect } from 'chai';
 
 describe('BaseManager', () => {
 
     let baseManager: BaseManager = new BaseManager();
+    let userManager: UserManager = new UserManager();
 
     describe('#create', () => {
         it('Should throw an error when invalid data', done => {
@@ -158,6 +161,7 @@ describe('BaseManager', () => {
                 baseManager.create(new Base('name2')).then(() => {
                     baseManager.create(new Base('name3')).then(() => {
                         baseManager.search('').then(bases => {
+                            console.log(bases);
                             expect(bases).to.exist;
                             expect(bases).to.be.an('array');
                             expect(bases).to.have.length(3);
@@ -203,6 +207,31 @@ describe('BaseManager', () => {
                                     expect(bases).to.have.length(2);
 
                                     done();
+                                });
+                            });
+                        });
+                    });
+                });
+            });
+        });
+
+        it('Should return base with amount of users', done => {
+            baseManager.create(new Base('Dolphin')).then((base1: Base) => {
+                baseManager.create(new Base('Misha')).then((base2: Base) => {
+                    userManager.create(new User('12', '22', 'id1', '42')).then(() => {
+                        userManager.create(new User('1', '2', 'id2', '4')).then(() => {
+                            userManager.setBase('id1', base1).then(() => {
+                                userManager.setBase('id2', base1).then(() => {
+                                    baseManager.search('dol').then(bases => {
+                                        expect(bases).to.exist;
+                                        expect(bases).to.be.an('array');
+                                        expect(bases).to.have.length(1);
+                                        expect(bases[0]).to.have.property('name', 'Dolphin');
+                                        expect(bases[0]).to.have.property('users');
+                                        expect(bases[0].users).to.eql(2);
+
+                                        done();
+                                    })
                                 });
                             });
                         });
