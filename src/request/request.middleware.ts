7adea5c1745ae2a2f0735202ab0765927270c2c7
Ron Borysovski <roni537@gmail.com>
2017-04-10 12:51:46 +0300

    Added canEdit and canDelete request middlewares

--- src/request/request.middleware.ts
+++ src/request/request.middleware.ts
@@ -1,21 +1,45 @@
 import * as express from 'express';
+import { Types } from 'mongoose';
 import { IUser } from "../user/user.interface";
-export class RequestsMiddleware {
-    public static requireRequestOwner(req: express.Request, res: express.Response, next: express.NextFunction) {
-        let request = req.body.request;
-        let user: IUser = req.user;
+import { Request } from './request.class';
+import { IRequest } from "./request.interface";
 
-        if (!user) {
-            return res.redirect('/login');
+export class RequestsMiddleware {
+    public static canEdit(req: express.Request, res: express.Response, next: express.NextFunction) {
+        let requestId: Types.ObjectId = null;
+        let user = <IUser>req.user;
+
+        try {
+            requestId = new Types.ObjectId(req.params['id']);
+        } catch (err) {
+            return res.sendStatus(400);
         }
 
-        if (request && request.requestor) {
+        Request.findRequest(requestId).then((request: IRequest) => {
             if (request.requestor._id === user._id) {
                 return next();
             }
-            return res.status(403).send('Only request owner is permitted to edit the request');
+
+            return res.sendStatus(403);
+        });
+    }
+
+    public static canDelete(req: express.Request, res: express.Response, next: express.NextFunction) {
+        let requestId: Types.ObjectId = null;
+        let user = <IUser>req.user;
+
+        try {
+            requestId = new Types.ObjectId(req.params['id']);
+        } catch (err) {
+            return res.sendStatus(400);
         }
 
-        return res.status(400).send();
+        Request.findRequest(requestId).then((request: IRequest) => {
+            if (request.requestor._id === user._id) {
+                return next();
+            }
+
+            return res.sendStatus(403);
+        });
     }
 }
