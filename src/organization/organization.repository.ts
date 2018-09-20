ad1849230ad407c844b7b29e0c18611cf4219198
Ron Borysovski <roni537@gmail.com>
2018-09-20 19:40:58 +0300

    refactor: Refactor code and fixed some filters

--- src/organization/organization.repository.ts
+++ src/organization/organization.repository.ts
@@ -1,12 +1,13 @@
 import { ITask } from './../workflow/task.interface';
 import { Types, Document } from 'mongoose';
-import * as Promise from 'bluebird';
 import { RepositoryBase } from "../helpers/repository";
 import { IOrganization } from "./organization.interface";
 import { OrganizationModel } from "./organization.model";
 import { ICollection } from "../helpers/collection";
 import { IPaginationOptions } from "../pagination/pagination.interface";
 import { TaskType } from "../workflow/task-type.enum";
+import { UserModel } from "../user/user.model";
+import { RequestModel } from "../request/request.model";
 
 
 export class OrganizationRepository extends RepositoryBase<IOrganization> {
@@ -14,45 +15,64 @@ export class OrganizationRepository extends RepositoryBase<IOrganization> {
         super(OrganizationModel);
     }
 
-    search(searchTerm: string, paginationOptions?: IPaginationOptions): Promise<ICollection<IOrganization>> {
-        return new Promise<ICollection<IOrganization>>((resolve, reject) => {
-            let searchTermCaseInsensitiveRegex = new RegExp(searchTerm, 'i');
+    async search(searchTerm: string, paginationOptions?: IPaginationOptions): Promise<ICollection<IOrganization>> {
+        let searchTermCaseInsensitiveRegex = new RegExp(searchTerm, 'i');
 
-            let countPromise = OrganizationModel.count({ name: searchTermCaseInsensitiveRegex });
-            let organizationsPromise = OrganizationModel.aggregate().match({ name: searchTermCaseInsensitiveRegex }).lookup({
-                from: "users",
-                localField: "_id",
-                foreignField: "organization",
-                as: "users"
-            }).lookup({
-                from: "requests",
-                localField: "_id",
-                foreignField: "organization",
-                as: "requests"
-            }).project({
-                name: 1,
-                users: { $size: "$users" },
-                requests: { $size: "$requests" }
-            });
+        let countPromise = OrganizationModel.count({ name: searchTermCaseInsensitiveRegex });
+        let organizationsPromise = OrganizationModel.find({ name: searchTermCaseInsensitiveRegex });
+        // let organizationsPromise = OrganizationModel.aggregate().match({ name: searchTermCaseInsensitiveRegex }).lookup({
+        //     from: "users",
+        //     localField: "_id",
+        //     foreignField: "organization",
+        //     as: "users"
+        // }).lookup({
+        //     from: "requests",
+        //     localField: "_id",
+        //     foreignField: "organization",
+        //     as: "requests"
+        // }).project({
+        //     name: 1,
+        //     showRequests: 1,
+        //     canCreateRequests: 1,
+        //     users: { $size: "$users" },
+        //     requests: { $size: "$requests" }
+        // }).allowDiskUse(true);
 
-            if (paginationOptions) {
-                organizationsPromise = organizationsPromise.skip(paginationOptions.skip).limit(paginationOptions.limit);
-            }
+        if (paginationOptions) {
+            organizationsPromise = organizationsPromise.skip(paginationOptions.skip).limit(paginationOptions.limit);
+        }
 
-            Promise.all([countPromise, organizationsPromise]).then(values => {
-                let result = {
-                    totalCount: values[0],
-                    set: <IOrganization[]>values[1]
-                };
+        let [organizations, count] = await Promise.all([organizationsPromise, countPromise]);
 
-                resolve(result);
-            }).catch(err => {
-                reject(err);
-            });
+        const organizationIds = organizations.map((org: IOrganization) => org._id);
+
+        const countForOrganization = await Promise.all(organizationIds.map(async id => {
+            const userCount = await UserModel.count({ organization: id });
+            const requestCount = await RequestModel.count({ organization: id });
+
+            return {
+                id,
+                userCount,
+                requestCount
+            };
+        }));
+
+        organizations = organizations.map((organization: IOrganization) => {
+            let orgData = countForOrganization.find(org => org.id.equals(organization._id));
+            Object.assign(organization, { users: orgData.userCount, requests: orgData.requestCount });
+
+            return organization;
         });
+
+        let result = <ICollection<IOrganization>>{
+            set: organizations,
+            totalCount: count
+        };
+
+        return result;
     }
 
-    setWorkflow(organizationId: Types.ObjectId, workflow: ITask[]): Promise<Document> {
+    async setWorkflow(organizationId: Types.ObjectId, workflow: ITask[]): Promise<Document> {
 
         if (workflow) {
 
@@ -76,26 +96,28 @@ export class OrganizationRepository extends RepositoryBase<IOrganization> {
                         organizationsToUpdate.push(task.organization);
                     }
                 });
+            } else { // current organization not in workflow. need to update every other organization
+                workflow.forEach(task => {
+                    if (task.type === TaskType.HUMAN) {
+                        organizationsToUpdate.push(task.organization);
+                    }
+                });
             }
 
-            return OrganizationModel.update({}, { $pull: { tags: { _id: organizationId } } }, { multi: true }).exec().then(() => {
-                organizationsToUpdate = organizationsToUpdate.map(org => org._id);
-                return OrganizationModel.update({ _id: { $in: organizationsToUpdate } }, { $addToSet: { tags: { _id: organizationId } } }, { multi: true }).exec();
-            }).then(() => {
-                return this.update(organization);
-            });
+            await OrganizationModel.update({}, { $pull: { tags: organizationId } }, { multi: true }).exec();
+            organizationsToUpdate = organizationsToUpdate.map(org => org._id);
+            await OrganizationModel.update({ _id: { $in: organizationsToUpdate } }, { $addToSet: { tags: organizationId } }, { multi: true }).exec();
+            return await this.update(organization);
+
         } else {
-            return Promise.resolve(null);
+            return null;
         }
 
     }
 
-    getWorkflow(organizationId: Types.ObjectId): Promise<ITask[]> {
-        return new Promise<ITask[]>((resolve, reject) => {
-            this.findById(organizationId, { path: 'workflow.organization', select: 'name' }).then((organization: IOrganization) => {
-                resolve(organization ? organization.workflow : null);
-            }).catch(reject);
-        });
+    async getWorkflow(organizationId: Types.ObjectId): Promise<ITask[]> {
+        let organization = <IOrganization>await this.findById(organizationId, { path: 'workflow.organization', select: 'name' });
+        return organization ? organization.workflow : null;
     }
 
     private getUniqueTasks(workflow: ITask[]): ITask[] {
