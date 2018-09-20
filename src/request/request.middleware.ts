73688886a988711a553d51f6005d6bc8941123d6
Ron Borysovski <roni537@gmail.com>
2018-09-20 19:43:51 +0300

    refactor: Use async/await instead of Promise.resolve/reject

--- src/request/request.middleware.ts
+++ src/request/request.middleware.ts
@@ -1,13 +1,13 @@
-import { Permission } from './../permission/permission.class';
-import { PermissionType } from './../permission/permission.enum';
 import * as express from 'express';
 import { Types } from 'mongoose';
 import { IUser } from "../user/user.interface";
+import { Permission } from './../permission/permission.class';
+import { PermissionType } from './../permission/permission.enum';
 import { Request } from './request.class';
 import { IRequest } from "./request.interface";
 
 export class RequestsMiddleware {
-    public static canEdit(req: express.Request, res: express.Response, next: express.NextFunction) {
+    public static async canEdit(req: express.Request, res: express.Response, next: express.NextFunction) {
         let requestId: Types.ObjectId = null;
         let user = <IUser>req.user;
 
@@ -17,37 +17,35 @@ export class RequestsMiddleware {
             return res.sendStatus(400);
         }
 
-        Request.findRequest(requestId).then((request: IRequest) => {
-            if (request.requestor._id === user._id) {
-                return next();
-            }
+        let request = <IRequest>await Request.findRequest(requestId);
+        if (request && request.requestor._id === user._id) {
+            return next();
+        }
 
-            return res.sendStatus(403);
-        });
+        return res.status(403).send();
     }
 
-    public static canDelete(req: express.Request, res: express.Response, next: express.NextFunction) {
+    public static async canDelete(req: express.Request, res: express.Response, next: express.NextFunction) {
         let requestId: Types.ObjectId = null;
         let user = <IUser>req.user;
 
         try {
             requestId = new Types.ObjectId(req.params['id']);
         } catch (err) {
-            return res.sendStatus(400);
+            return res.status(400).send();
         }
 
-        Request.findRequest(requestId).then((request: IRequest) => {
-            if (request.requestor._id === user._id) {
-                return next();
-            }
+        let request = <IRequest>await Request.findRequest(requestId);
+        if (request && request.requestor._id === user._id) {
+            return next();
+        }
 
-            Permission.hasPermissionForOrganization(user._id, [PermissionType.DELETE_REQUEST], request.organization._id).then((hasPermission: boolean) => {
-                if (hasPermission) {
-                    return next();
-                }
+        let hasPermission: boolean = await Permission.hasPermissionForOrganization(user._id,
+            [PermissionType.DELETE_REQUEST], request.organization._id);
+        if (hasPermission) {
+            return next();
+        }
 
-                return res.sendStatus(403);
-            });
-        })
+        return res.status(403).send();
     }
 }
